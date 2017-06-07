fc5b2f2d05ea166d2841ca0c1611f5e825f8bd2e
Aubrey Masten and Grady Shelton <grady@>
2017-06-07 11:47:33 -0700

    customer table

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,10 @@ import { Keg } from './keg.model';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
+  <div class="container-fluid">
     <h1>Kombucha Boyz</h1>
     <div class="row">
-      <div class="col-md-6"><customer></customer></div>
+      <div class="col-md-6"><customer [childKegList]="masterKegList"></customer></div>
       <div class="col-md-6"><employee [childKegList]="masterKegList"></employee></div>
     </div>
 
--- app/customer.component.ts
+++ app/customer.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Keg } from './keg.model'
 
 @Component({
   selector: 'customer',
   template: `
   <h3>Customer Side</h3>
+  <table class='table'>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Brand</th>
+        <th>Price</th>
+        <th>Flavor</th>
+        <th>Remaining Pints</th>
+      </tr>
+    </thead>
+    <tbody>
+  <tr *ngFor='let currentKeg of childKegList'>
+
+    <td>{{currentKeg.name}}</td>
+    <td>{{currentKeg.brand}}</td>
+    <td>{{currentKeg.price}}</td>
+    <td>{{currentKeg.flavor}}</td>
+    <td>{{currentKeg.pints}}</td>
+  </tr>
+  </tbody>
+  </table>
   `
 })
 
 export class CustomerComponent {
+  @Input() childKegList: Keg[];
 
 }
